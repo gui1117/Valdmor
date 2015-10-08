f45f7366459de21566e5f546f103d04d3fadddaf
thiolliere <guillaume.thiolliere@voila.fr>
2015-10-08 22:25:01 +0200

    flooder

---
+++ src/flooder.js
@@ -0,0 +1,10 @@
+/* each room has a flooder spawn.
+ * when the alarm happen, the room where
+ * the character is and the two aside create
+ * flooder on their spawn.
+ *
+ * a flooder is an entity that go forward and
+ * chose any intersection randomly (but no backward)
+ * it lives in th 3-room area (for performance purpose)
+ * (or in a certain distance of character)
+ */
