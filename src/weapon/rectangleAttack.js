53cabe13994b416219c2d895e68adc5624dff75b
thiolliere <guillaume.thiolliere@voila.fr>
2015-10-10 23:12:50 +0200

    .

---
+++ src/weapon/rectangleAttack.js
@@ -0,0 +1,14 @@
+function rectangleAttack(spec) {
+	var type = shape.type,
+	width = shape.width,
+	height = shape.height,
+	immune = spec.immune || [],
+	damage = spec.damage,
+	rotation = spec.rotation || 0,
+	mask = spec.mask,
+	division = spec.division || 1;
+
+
+
+
+}
